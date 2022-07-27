098cee8d26466267dbbbb3b3f1533009dad989e6
hsl <huangshaoli@qq.com>
2022-07-27 16:55:18 +0800

    second commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -2,6 +2,7 @@ package com.atguigu;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
